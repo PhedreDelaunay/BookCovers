8689ea00495022055165ccf2f67fa983802c23e2
Nicola Tarbet <phedre@galaga.plus.com>
2018-08-18 13:20:54 +0100

    add django application files and directories created for bookcovers app

---
+++ bookcovers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+# https://docs.djangoproject.com/en/2.0/ref/applications/
+# To configure an application, subclass AppConfig and put the dotted path to that subclass in INSTALLED_APPS.
+class BookcoversConfig(AppConfig):
+    name = 'bookcovers'
