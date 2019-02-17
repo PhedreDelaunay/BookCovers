a0e35d8af0fd0b23c76a44932492207d25abeba1
Phedre Delaunay <phedre@galaga.plus.com>
2019-02-17 06:23:29 +0000

    admin pages

--- bookcovers/admin.py
+++ bookcovers/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from bookcovers.models import Authors
+
 # Register your models here.
+
+# https://djangobook.com/customizing-change-lists-forms/
+
+class AuthorAdmin(admin.ModelAdmin):
+    # list_display = ('name')
+    # add a search bar
+    search_fields = ['name',]
+
+admin.site.register(Authors, AuthorAdmin)
