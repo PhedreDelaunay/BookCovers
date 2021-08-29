6388c617c9359a7427d12b897909a83250391ce3
Phedre Delaunay <phedre@galaga.plus.com>
2021-08-29 10:21:27 +0100

    add order to artbook index]

--- update_migrations.sh
+++ update_migrations.sh
@@ -2,7 +2,11 @@
 # update_migrations
 
 
+# 29-Aug-2021
+cp bookcovers/migrations/0006_auto_20210829_0912.py ~/djabbic/bookcovers/migrations/
+~/init/djabbic restart
+
 # 25-Apr-2021
-cp bookcovers/migrations/0005_auto_20210425_1349.py ~/djabbic/bookcovers/migrations/
+#cp bookcovers/migrations/0005_auto_20210425_1349.py ~/djabbic/bookcovers/migrations/
 #~/init/djabbic restart
 
